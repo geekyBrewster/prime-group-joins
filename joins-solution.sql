41f8ce8e33c12458671a519ecde3ba260709066f
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-11 15:31:48 -0500

    Count and group by (num. 5) now works.

--- joins-solution.sql
+++ joins-solution.sql
@@ -22,7 +22,15 @@ JOIN warehouse ON warehouse.id = warehouse_product.warehouse_id
 WHERE products.description ILIKE 'diet pepsi';
 
 -- 5. Get the number of orders for each customer
--- ???????
+SELECT customers.first_name, count(orders.address_id) FROM orders
+JOIN addresses ON addresses.id = orders.address_id
+JOIN customers ON customers.id = addresses.customer_id
+GROUP BY customers.first_name;
+-- First, we joined the addresses w/ orders tables
+-- Second, we joined on the customers table == Building the overall table that we had to count from
+-- Next, figured we wanted to select by customer name (used first name)
+-- Counting by address for each person
+-- Grouped by first name (or what we're selecting by)
 
 -- 6. How many customers do we have?
 SELECT count(customers.id) FROM customers;
